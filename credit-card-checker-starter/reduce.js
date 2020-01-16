cf8879438b0a294446f200b001923f10388ed1ea
deepfunk9 <deepfunk9@hotmail.com>
2020-01-16 21:49:41 +0000

    stuff

---
+++ credit-card-checker-starter/reduce.js
@@ -0,0 +1,17 @@
+const array1 = [1, 2, 3, 4];
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+// 1 + 2 + 3 + 4
+console.log(array1.reduce(reducer));
+// expected output: 10
+
+// 5 + 1 + 2 + 3 + 4
+console.log(array1.reduce(reducer, 5));
+// expected output: 15
+
+let total = [ 0, 1, 2, 3 ].reduce(
+    ( accumulator, currentValue ) => accumulator + currentValue,
+    0
+  )
+
+console.log(total);
